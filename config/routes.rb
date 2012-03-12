558d890cba7146a29454ba644aa5f2d939be5e66
Enes Selmanovic <eselmanovic@devlogic.eu>
2012-03-12 15:23:59 +0100

    Add styling and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 SimpleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-  
-  get "pages/about"
+  get "users/new"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -11,6 +7,12 @@ SimpleApp::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  
+  match '/signup', :to => 'users#new'
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
@@ -54,7 +56,7 @@ SimpleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+    root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
